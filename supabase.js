a277ae7b21588b7784571cb23608a6a150add22f
antnish1 <shahdolho@gmail.com>
2026-04-04 23:34:30 +0530

    Create supabase.js
    Signed-off-by: antnish1 <shahdolho@gmail.com>

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://gmutgbdldiqbwomtdepi.supabase.co";
+const SUPABASE_KEY = "sb_publishable_e-gFkBqs2qG2bSs1iBJPrQ_m3PZf5lN";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
